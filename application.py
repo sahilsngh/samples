953f8361c5325a77d424c718593c444b8b3de8e9
Sahil Singh <sahilraaj745@gmail.com>
2021-10-25 23:55:17 +0530

    Add files via upload

---
+++ application.py
@@ -0,0 +1,28 @@
+from flask import Flask
+import json
+import requests
+
+application = Flask(__name__)
+
+
+# with open('./final.json') as f:
+#   data = json.load(f)
+
+url = 'https://raw.githubusercontent.com/sahilsngh/samples/main/final.json'
+resp = requests.get(url)
+data = json.loads(resp.text)
+# print(data)
+
+
+@application.route('/<variable>', methods=['GET'])
+def return_metadata(variable):
+	# return {"new": f"file {variable}"}
+	metadata = data[variable]
+	return metadata
+
+@application.route('/')
+def index():
+	return "Please use endpoint as https://xyz/||PLACEHOLDER|| \n  ---------------> replace ||PLACEHOLDER|| with the integer value ranging from 1 to 10000 for metadata"
+
+if __name__ == '__main__':
+	application.run(debug=True)
