cbb5047941a1141122e79d905967410612033442
sahilsngh <sahilraaj745@gmail.com>
2021-10-26 03:32:47 +0530

    replaced schema with metadata

--- application.py
+++ application.py
@@ -8,20 +8,28 @@ application = Flask(__name__)
 # with open('./final.json') as f:
 #   data = json.load(f)
 
-url = 'https://raw.githubusercontent.com/sahilsngh/samples/main/final.json'
-resp = requests.get(url)
-data = json.loads(resp.text)
+# url = 'https://raw.githubusercontent.com/sahilsngh/samples/main/final.json'
+# resp = requests.get(url)
+# data = json.loads(resp.text)
 # print(data)
 
 # Access Route
 
-@application.route('/<variable>', methods=['GET'])
-def return_metadata(variable):
-	# return {"new": f"file {variable}"}
-	metadata = data[variable]
-	return metadata
-# Default Route
+# @application.route('/<variable>', methods=['GET'])
+# def return_metadata(variable):
+# 	# return {"new": f"file {variable}"}
+# 	metadata = data[variable]
+# 	return metadata
 
+@application.route('/<variable>', methods=['GET'])
+def return_on_each_call(variable):
+	github_url = f'https://raw.githubusercontent.com/sahilsngh/samples/main/schema/{variable}.json'
+	response = requests.get(github_url)
+	metadata = json.loads(response.text)
+	return metadata
+
+
+# Default Route
 @application.route('/')
 def index():
 	return "Please use endpoint as https://xyz/||PLACEHOLDER|| \n  ---------------> replace ||PLACEHOLDER|| with the integer value ranging from 1 to 10000 for metadata"
